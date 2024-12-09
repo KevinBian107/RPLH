df390f0d87de823c5b30189b3c86f5836fdba9f7
Kevin Bian <kaiwenbian107@gmail.com>
2024-12-09 13:55:00 -0800

    debuged

--- rplh/evaluation/embed.py
+++ rplh/evaluation/embed.py
@@ -80,6 +80,7 @@ def get_spy_detect_embedding(main_df,
                              spy_df,
                              justification_df, 
                              spy_sentence="you are the spy agent, your goal is to prevent match targets",
+                             just_sentence="I suspect that this agent is the spy agent, thus, I would not listen to this agent.",
                              spy_agents=["Agent[0.5, 0.5]", "Agent[1.5, 1.5]", "Agent[2.5, 2.5]"]):
     '''Get embeddings for spy data'''
     
@@ -91,6 +92,6 @@ def get_spy_detect_embedding(main_df,
             spy_embed = spy_df[col].apply(lambda x: cosine_sim(get_embedding(spy_sentence), get_embedding(str(x))))
             out_df[f"Spy_Embed_{col}"] = spy_embed
     
-    out_df['Justification_Embed'] = justification_df['Justifications'].apply(lambda x: cosine_sim(get_embedding(spy_sentence), get_embedding(str(x))))
+    out_df['Justification_Embed'] = justification_df['Justifications'].apply(lambda x: cosine_sim(get_embedding(just_sentence), get_embedding(str(x))))
     
     return out_df
